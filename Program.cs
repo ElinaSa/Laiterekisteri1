079e10ac0886ec96faff4756fcfa6e9cf6f10f5b
ElinaSa <156064412+ElinaSa@users.noreply.github.com>
2024-02-24 19:46:07 +0200

    lisätty tietokoneelle alikuokka ja siihen konstruktori

--- Program.cs
+++ Program.cs
@@ -6,58 +6,44 @@ using System.Threading.Tasks;
 
 namespace Laiterekisteri
 {
-    // Base class for devices
+    // Base class for devices, yleinen laiteluokka, yliluokka tietokoneille, tableteille ja puhelimille
     class Device
     {
-        // Fields
+        // Fields, luodaan kenttä (field) identity, esitellään (define) ja annetaan arvo (set initial value)
         // ------
-        string identity = "Uusi laite";
+        string identity = "Uusi laite"; 
+        
+        // Luodaan kenttää vastaava ominaisuus (property) Identity ja sille asetusmetodi set ja lukumetodi get. Ne voi kirjoittaa joko yhdelle tai useammalle siriville
+        public string Identity { get { return identity; } set { identity = value; } }
+
         string dateBought = "1.1.2000";
+        public string DateBought { get { return dateBought; } set { dateBought = value; } }
+
+        // Huomaa jälkiliite d (suffix)
         double price = 0.00d;
+        public double Price { get { return price; } set { price = value; } }
+
         int warranty = 12;
+        public int Warranty { get { return warranty; } set { warranty = value; } }
 
         // Properties
         // ----------
-        public string Identity
-        {
-            get { return identity; }
-            set { identity = value; }
-
-        }
-
-        public string DateBought
-        {
-            get { return dateBought; }
-            set { dateBought = value; }
-        }
-
-        public double Price
-        {
-            get { return price; }
-            set { price = value; }
-        }
-
-        public int Warranty
-        {
-            get { return warranty; }
-            set { warranty = value; }
-        }
-
+           
+              
         // Constructors
         // ------------
-        // Kostruktori eli oliomuodostin (constructor) ilman argumentteja
-
+        // Kostruktori eli oliomuodostin (constructor) ilman argumentteja    
         public Device()
         {
 
         }
-        // A constructor with one argument
+        // A constructor with one argument, konstruktori nimi-argumentilla, tässä tapauksessa identiteetti
         public Device(string identity)
         {
             this.identity = identity;
         }
 
-        // Another constructor with all arguments
+        // Another constructor with all arguments, konstrutori kaikilla argumenteilla
         public Device(string identity, string dateBought, double price, int warranty)
         {
             this.identity = identity;
@@ -71,27 +57,26 @@ namespace Laiterekisteri
     }
 
     // Class for computers, inherits Device class
+    // Tietokoneiden luokka, perii ominaisuuksia ja metodeja laiteluokasta Device
     class Computer : Device
     {
-        // Fields
-        string identity = "Tietokone1";
-        string dateBought = "2.2.2022";
-        double price = 0.00d;
-        int warranty = 12;
-            
-       
+        // Fields, kentät ja Properties, ominaisuudet
+        string processorType;
+        public string ProcessorType { get { return processorType; } set { processorType = value; } }
 
-                    
-        // Properties
-       
-           
-        // Constructors
-       
-        // Other methods
-    }
+        int amountRAM;       
+        public int AmountRAM { get { return amountRAM; } set { amountRAM = value; } }
 
+        int storageCapacity;
+        public int StorageCapacity { get { return storageCapacity; } set { storageCapacity = value; } }
+                
+        // Konstruktorit
+        public Computer() : base()
+        { }
+    }    
     // Class for computers, inherits Device class
     class SmartPhone : Device
+
     {
         // Fields
 
@@ -120,25 +105,24 @@ namespace Laiterekisteri
         static void Main(string[] args) 
         {
             // Ohjelman varsinaiset toiminnot tapahtuvat täällä
-            // Lets's create an test object from the Device class with default constructor (0 parameters)
-            Device device1 = new Device();
-            Console.WriteLine(device1.Identity);
+            // Ohjelma kysyy käyttäjältä tietoja laitteista ja vastaamalla kysymyksiin tiedot tallennetaan muuttujiin.
 
-            // Let's create another device with identity
-            Device device2 = new Device("Toinen laite");
-            Console.WriteLine(device2.Identity);
+            // Luodaan uusi laite Device-luokasta
 
-            // Let's create one more device with all parameters
-            Device device3 = new Device("Kolmas kone", "8.2.2024", 150.00d, 36);
-            Console.WriteLine(device3.Identity);
-            Console.WriteLine(device3.DateBought); 
-            Console.WriteLine(device3.Price);
-            Console.WriteLine(device3.Warranty);
+            Device laite = new Device("Eka kone");
+            Console.WriteLine("Laitteen nimi on: " + laite.Identity);
+            Console.WriteLine("Ostopäivä oli: " + laite.DateBought);
 
+            // Luodaan uusi tietokone, joka perii laiteluokan (Device) ominaisuudet ja metodit
+
+            Computer tietokone1 = new Computer();
+            Console.WriteLine("Uuden tietokoneen nimi on: " + tietokone1.Identity);
+            
+            // Pidetään ikkuna auki, kunnes käyttäjä painaa enteriä
             Console.ReadLine();
         }
     }
- }         
+}         
                      
     
 
