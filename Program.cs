a8dcb2323282176a0c29e29213d6047198486e5f
ElinaSa <156064412+ElinaSa@users.noreply.github.com>
2024-02-26 17:51:47 +0200

    Lisättiin yhteisiä tietoja

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace Laiterekisteri
         // ------
         string identity = "Uusi laite"; 
         
-        // Luodaan kenttää vastaava ominaisuus (property) Identity ja sille asetusmetodi set ja lukumetodi get. Ne voi kirjoittaa joko yhdelle tai useammalle siriville
+        // Luodaan kenttää vastaava ominaisuus (property) Identity ja sille asetusmetodi set ja lukumetodi get.
+        // Ne voi kirjoittaa joko yhdelle tai useammalle riville.
         public string Identity { get { return identity; } set { identity = value; } }
 
         string dateBought = "1.1.2000";
@@ -26,10 +27,20 @@ namespace Laiterekisteri
         int warranty = 12;
         public int Warranty { get { return warranty; } set { warranty = value; } }
 
+        string processorType = "N/A";
+        public string ProcessorType { get { return processorType; } set { processorType = value; } }
+
+        int amountRAM = 0;
+        public int AmountRAM { get { return amountRAM; } set { amountRAM = value; } }
+
+        int storageCapacity =0;
+        public int StorageCapacity { get { return storageCapacity; } set { storageCapacity = value; } }
+
+
         // Properties
         // ----------
-           
-              
+
+
         // Constructors
         // ------------
         // Kostruktori eli oliomuodostin (constructor) ilman argumentteja    
@@ -60,19 +71,22 @@ namespace Laiterekisteri
     // Tietokoneiden luokka, perii ominaisuuksia ja metodeja laiteluokasta Device
     class Computer : Device
     {
-        // Fields, kentät ja Properties, ominaisuudet
-        string processorType;
-        public string ProcessorType { get { return processorType; } set { processorType = value; } }
-
-        int amountRAM;       
-        public int AmountRAM { get { return amountRAM; } set { amountRAM = value; } }
-
-        int storageCapacity;
-        public int StorageCapacity { get { return storageCapacity; } set { storageCapacity = value; } }
-                
+       
         // Konstruktorit
         public Computer() : base()
-        { }
+            { }
+
+        public Computer(string identity) : base(identity)
+            { }
+
+        // Muut metodit
+        public void ShowInfo()
+            {
+            Console.WriteLine("Koneen nimi: " + Identity);
+            Console.WriteLine("Prosessori: " + ProcessorType);
+            Console.WriteLine("Keskusmuisti: " + AmountRAM);
+            Console.WriteLine("Levytila: " + StorageCapacity);
+            }
     }    
     // Class for computers, inherits Device class
     class SmartPhone : Device
@@ -87,16 +101,14 @@ namespace Laiterekisteri
         // Other methods
     }
 
-    // Class for computers, inherits Device class
+    // Class for Tablets, inherits Device class, Tablettien luokka, perii laiteluokan
     class Tablet : Device
     {
-        // Fields
-
-        // Properties
-
-        // Constructors
-
-        // Other methods
+        // Fields, kentät
+        string operatingSystem;
+        public string OperatingSystem { get { return operatingSystem; } set { operatingSystem = value; } }
+        bool stylusEnabled = false;
+        public bool StylusEnabled { get {  return stylusEnabled; } set {  stylusEnabled = value; } }
     }
    
     // Pääluokan ohjelma, josta tulee Program.exe
@@ -108,16 +120,25 @@ namespace Laiterekisteri
             // Ohjelma kysyy käyttäjältä tietoja laitteista ja vastaamalla kysymyksiin tiedot tallennetaan muuttujiin.
 
             // Luodaan uusi laite Device-luokasta
-
             Device laite = new Device("Eka kone");
             Console.WriteLine("Laitteen nimi on: " + laite.Identity);
             Console.WriteLine("Ostopäivä oli: " + laite.DateBought);
 
             // Luodaan uusi tietokone, joka perii laiteluokan (Device) ominaisuudet ja metodit
-
             Computer tietokone1 = new Computer();
-            Console.WriteLine("Uuden tietokoneen nimi on: " + tietokone1.Identity);
-            
+
+            // Asetetaan Prosessori-ominaisuuden arvo
+            tietokone1.ProcessorType = "Intel I7";
+            tietokone1.AmountRAM = 16;
+
+            Console.WriteLine("Uuden tietokoneen nimi on: " + tietokone1.Identity + " ja siinä on " + tietokone1.ProcessorType + " -prosessori ja " + tietokone1.AmountRAM + " GB keskusmuistia");
+
+            // Luodaan uusi nimetty tietokone toisella konstruktorilla
+            Computer tietokone2 = new Computer("Elina läppäri");
+            tietokone2.ProcessorType = "Intel Core i7 vPro";
+            tietokone2.AmountRAM = 32;
+            tietokone2.ShowInfo();
+
             // Pidetään ikkuna auki, kunnes käyttäjä painaa enteriä
             Console.ReadLine();
         }
